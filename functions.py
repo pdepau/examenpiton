9d2abd8132f1e072d5ae3047cec03befe6e4c995
pdepau <blanespau@gmail.com>
2023-03-22 10:45:53 +0100

    commit_3

---
+++ functions.py
@@ -0,0 +1,13 @@
+import csv
+import math
+def read_data(datos):
+    fichero = open(datos, mode="rt", encoding="utf-8")
+
+def split(diccionario):
+    print()
+
+def reduce(diccionario,atributo):
+    print()
+
+def silhouette(lista1,lista2):
+    print()
